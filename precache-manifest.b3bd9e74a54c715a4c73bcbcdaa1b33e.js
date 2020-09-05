c167e8d89ad9603daf6131946bcbd05744d35de8
Pinki <pinki.verma17.pv@gmail.com>
2020-09-05 06:09:10 +0530

    Updates

---
+++ precache-manifest.b3bd9e74a54c715a4c73bcbcdaa1b33e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768b74540e96076dadb998e76b57b8b6",
+    "url": "/reactdemowebsite/index.html"
+  },
+  {
+    "revision": "bd879438bde892af6c15",
+    "url": "/reactdemowebsite/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "df28f076fa702639ed1a",
+    "url": "/reactdemowebsite/static/css/main.99054c73.chunk.css"
+  },
+  {
+    "revision": "bd879438bde892af6c15",
+    "url": "/reactdemowebsite/static/js/2.589a695e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactdemowebsite/static/js/2.589a695e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df28f076fa702639ed1a",
+    "url": "/reactdemowebsite/static/js/main.7990337f.chunk.js"
+  },
+  {
+    "revision": "98d4f7bc4cb7b24d231d",
+    "url": "/reactdemowebsite/static/js/runtime-main.d2ade41d.js"
+  }
+]);
